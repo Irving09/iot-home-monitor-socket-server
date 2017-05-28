a8f38aa281cec5e4134a7257ad08dbe074091372
Inno Irving V. Estrera <inno.irving.estrera@gmail.com>
2017-05-27 23:46:19 -0700

    Added dummy template, endpoint and message topic for other clients to communicate with

--- src/index.js
+++ src/index.js
@@ -2,19 +2,21 @@ const app   = require('express')();
 const http  = require('http').Server(app);
 const io    = require('socket.io')(http);
 
+const MESSAGE_TOPIC = 'iot-home-intruder';
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
   console.log('a user connected');
-  socket.on('add-message', (message) => {
-  console.log('=================');
-  console.log('this is server-side');
-  console.log(`received message from client side: ${message}`);
-  console.log('=================');
 
-  socket.emit('add-message', 'inno-server was here dough');
+  socket.on(MESSAGE_TOPIC, (message) => {
+    console.log('=================');
+    console.log(`received message: ${message}`);
+    console.log('=================');
+
+    socket.emit(MESSAGE_TOPIC, message);
   });
 });
 
