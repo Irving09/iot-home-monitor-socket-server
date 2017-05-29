591486b2038566535d9fa601f9bb68438c001de0
Inno Irving V. Estrera <inno.irving.estrera@gmail.com>
2017-05-29 11:26:48 -0700

    added views using pug

--- src/index.js
+++ src/index.js
@@ -1,22 +1,43 @@
+'use strict';
+
 const app   = require('express')();
 const http  = require('http').Server(app);
 const io    = require('socket.io')(http);
 
 const MESSAGE_TOPIC = 'iot-home-intruder';
 
+app.set('view engine', 'pug');
+
+let test;
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  // res.sendFile(__dirname + '/index.html');
+  res.render('index', {
+    title: 'hey',
+    img: {
+      src: "image-stream.png"
+    }
+  });
+
+  test = res;
 });
 
 io.on('connection', (socket) => {
   console.log('a user connected');
 
-  socket.on(MESSAGE_TOPIC, (message) => {
+  socket.on(MESSAGE_TOPIC, (imageStream) => {
     console.log('=================');
-    console.log(`received message: ${message}`);
+    console.log(`received message:`);
     console.log('=================');
 
-    socket.emit(MESSAGE_TOPIC, message);
+    // ==> I want to re-set img src in index.html
+    test.render('index', {
+      title: 'hey',
+      img: {
+        src: 'data:image/png;base64,' + imageStream
+      }
+    });
+
   });
 });
 
