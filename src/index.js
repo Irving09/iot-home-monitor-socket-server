d3cf18b427318cb6381b5fc8968e1184a38cc7da
Inno Irving V. Estrera <inno.irving.estrera@gmail.com>
2017-05-31 20:50:24 -0700

    Added aws-iot-device-sdk and allowed cors for ui to make calls to the put api

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 'use strict';
 
 const app   = require('express')();
+const cors  = require('cors');
 const http  = require('http').Server(app);
 const io    = require('socket.io')(http);
+const iot   = require('aws-iot-device-sdk');
 
 const MESSAGE_TOPIC = 'iot-home-intruder';
 const UI_TOPIC = 'iot-home-intruder-ui';
 
+const Device = iot.device(require('./credentials'));
+
 app.set('view engine', 'pug');
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.render('index', {
@@ -15,15 +20,28 @@ app.get('/', (req, res) => {
   });
 });
 
+app.put('/stream', function (req, res) {
+  console.log('inside socket server');
+  console.log(req);
+  res.send({
+    test: 'hello from socket server'
+  });
+});
+
 io.on('connection', function(socket) {
   console.log('a user connected');
 
   socket.on(MESSAGE_TOPIC, function(imageStream) {
-    console.log(`received image stream`);
-    // TODO Emit imageStream to UI clients
     socket.broadcast.emit(UI_TOPIC, imageStream);
   });
 
+});
+
+Device.on('connect', (err, data) => {
+  if (err) throw err;
+  console.log('iot-home-monitor successfully connected to AWS IoT Device Gateway');
+
+  // Device.publish('start-stream', startStreaming);
 });
 
 http.listen(3000, () => console.log('listening on *:3000'));
