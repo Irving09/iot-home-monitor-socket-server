7d487a0eac5f03b8f91b7d656a91d56568348475
Inno Irving V. Estrera <inno.irving.estrera@gmail.com>
2017-06-01 08:54:13 -0700

    Reverting to stable state

--- src/index.js
+++ src/index.js
@@ -4,12 +4,9 @@ const app   = require('express')();
 const cors  = require('cors');
 const http  = require('http').Server(app);
 const io    = require('socket.io')(http);
-const iot   = require('aws-iot-device-sdk');
 
 const MESSAGE_TOPIC = 'iot-home-intruder';
 const UI_TOPIC = 'iot-home-intruder-ui';
-
-const Device = iot.device(require('./credentials'));
 
 app.set('view engine', 'pug');
 app.use(cors());
@@ -20,14 +17,6 @@ app.get('/', (req, res) => {
   });
 });
 
-app.put('/stream', function (req, res) {
-  console.log('inside socket server');
-  console.log(req);
-  res.send({
-    test: 'hello from socket server'
-  });
-});
-
 io.on('connection', function(socket) {
   console.log('a user connected');
 
@@ -35,13 +24,6 @@ io.on('connection', function(socket) {
     socket.broadcast.emit(UI_TOPIC, imageStream);
   });
 
-});
-
-Device.on('connect', (err, data) => {
-  if (err) throw err;
-  console.log('iot-home-monitor successfully connected to AWS IoT Device Gateway');
-
-  // Device.publish('start-stream', startStreaming);
 });
 
 http.listen(3000, () => console.log('listening on *:3000'));
